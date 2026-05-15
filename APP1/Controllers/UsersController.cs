e6e6b2efc9943a1035b519d493076eca3f4f651a
QaisMallouh <qaismallouhgh@gmail.com>
2026-05-15 19:12:41 +0300

    Read

---
+++ APP1/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using APP1.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APP1.Controllers
+{
+    public class UsersController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public IActionResult Index()
+        {
+            var products = context.Users.ToList();
+            return View("Index",products);
+        }
+
+
+    }
+}
---
+++ APP1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APP1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APP1.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+       public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;database=MVC14;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
+        }
+    }
+}
